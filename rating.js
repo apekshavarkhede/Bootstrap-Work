a1bdb2fd5341dabc0581b4e269ddd812e539bad1
bhoomi5 <bhoomikadedaniya29@gmail.com>
2020-01-15 19:29:47 +0530

    first Commit

---
+++ rating.js
@@ -0,0 +1,17 @@
+//  Rating Initialization
+// $(document).ready(function() {
+//   $('#rateMe2').mdbRate();
+// });
+// function onClick(element) {
+//   document.getElementById("img01").src = element.src;
+//   document.getElementById("modal01").style.display = "block";
+// }
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+
+
+$(function changeImage() {
+
+    $('#sandle1').html()
+
+})
+
